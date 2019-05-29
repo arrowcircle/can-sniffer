2897672e3cfedbd28284e8afed0fa7ea7c8ba8f1
Oleg Bovykin <oleg.bovykin@gmail.com>
2019-05-29 22:03:28 +0300

    Enabled HV2 linear readings

--- parser.rb
+++ parser.rb
@@ -20,44 +20,49 @@ class LineProcessor
 
   COMMANDS = {
     '2F 0 32 1 1 0 0 0' => 'Start HV1 Command 1',
+    '2F 0 32 2 1 0 0 0' => 'Start HV2 Command 1',
     '60 0 32 1 0 0 0 0' => 'Response Start HV1 Command 1 OK',
     '40 0 30 1 0 0 0 0' => 'Start HV1 Command 2',
+    '40 0 30 2 0 0 0 0' => 'Start HV2 Command 2',
     '4F 0 30 1 16 0 0 0' => 'Response Start HV1 Command 2 OK',
     '4F 0 30 1 17 0 0 0' => 'Response Start HV1 Command 2 OK A1',
+    '4F 0 30 1 1F 0 0 0' => 'Response Start HV1 Command 2 OK A2',
     '4F 0 30 1 1F 0 0 0' => 'Response Start HV1 Command 2 OK A2',
     '60 0 25 21 0 0 0 0' => 'Unknown command',
     '40 8 10 0 0 0 0 0' => 'Request IGPD status',
     '43 8 10 0 49 47 50 32' => 'Response Status OK',
     '40 2 64 1 0 0 0 0' => 'Request HV1 log reading',
-    '40 2 64 3 0 0 0 0' => 'Request HV1 linear reading',
+    '40 2 64 3 0 0 0 0' => 'Request HV1 ADC reading',
     '40 2 64 6 0 0 0 0' => 'Request HV2 linear reading',
     '40 2 64 4 0 0 0 0' => 'Request HV2 log reading',
-    '40 2 64 2 0 0 0 0' => 'Request unknown reading 1',
+    '40 2 64 2 0 0 0 0' => 'Request linear reading 1',
     '2F 0 32 1 0 0 0 0' => 'Stop HV1',
     '1A 0 0 0' => 'Heartbeat of no changes 0',
     '1A 0 0 80' => 'Heartbeat of no changes 80',
     '1A 0 1 0' => 'Heartbeat of HV1 after C1 0',
     '1A 0 1 80' => 'Heartbeat of HV1 after C1 80',
-    '1A 0 3 0' => 'Heartbeat of !unknown6! 0',
-    '1A 0 3 80' => 'Heartbeat of !unknown6! 80'
+    '1A 0 3 0' => 'Heartbeat of ADC change 0',
+    '1A 0 3 80' => 'Heartbeat of ADC change 80',
     '1A 1 0 0' => 'Heartbeat HV1 Log change 0',
     '1A 1 0 80' => 'Heartbeat HV1 Log change 80',
-    '1A 2 0 0' => 'Heartbeat of unknown change 0',
-    '1A 2 0 80' => 'Heartbeat of unknown change 80',
-    '1A 3 0 0' => 'Heartbeat of ?Log and Unknown change? 0',
-    '1A 3 0 80' => 'Heartbeat of ?Log and Unknown change? 80',
-    '1A 4 0 0' => 'Heartbeat of Linear change 0',
-    '1A 4 0 80' => 'Heartbeat of Linear change 80',
+    '1A 2 0 0' => 'Heartbeat of linear change 0',
+    '1A 2 0 80' => 'Heartbeat of linear change 80',
+    '1A 3 0 0' => 'Heartbeat of Log and Linear change 0', # seen only 2 times
+    '1A 3 0 80' => 'Heartbeat of Log and Linear change 80',
+    '1A 4 0 0' => 'Heartbeat of ADC change 0',
+    '1A 4 0 80' => 'Heartbeat of ADC change 80',
     '1A 5 0 0' => 'Heartbeat of !unknown4! 0',
     '1A 5 0 80' => 'Heartbeat of !unknown4! 80',
     '1A 24 0 0' => 'Heartbeat of !unknown5! 0',
-    '1A 24 0 80' => 'Heartbeat of !unknown5! 80'
+    '1A 24 0 80' => 'Heartbeat of !unknown5! 80',
+    '1A 24 1 0' => 'Heartbeat of ADC Enable 0',
+    '1A 24 1 80' => 'Heartbeat of ADC Enable 80'
   }
 
   HEADERS = {
     '4B 2 64 1' => 'Response HV1 log reading',
-    '4B 2 64 2' => 'Response Unknown reading 1',
-    '4B 2 64 3' => 'Response HV1 linear reading',
+    '4B 2 64 2' => 'Response linear reading 1',
+    '4B 2 64 3' => 'Response HV1 ADC reading',
     '4B 2 64 4' => 'Response HV2 log reading',
     '4B 2 64 6' => 'Response HV2 linear reading'
   }
@@ -127,6 +132,7 @@ class LineProcessor
   end
 end
 
+puts "TS, ID, Data, Source, Command, Unscaled, Scaled"
 File.foreach(ARGV[0]).with_index do |line, line_num|
   l = LineProcessor.new(line)
   puts l.result if l.source != 'Unknown'
